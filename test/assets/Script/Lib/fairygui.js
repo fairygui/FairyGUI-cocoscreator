fb559a5ef96f33a4b62bf5b5175823c145ecd133
guzhu@fairygui.com <guzhu@fairygui.com>
2019-01-07 17:42:15 +0800

    previous commit mistake

--- test/assets/Script/Lib/fairygui.js
+++ test/assets/Script/Lib/fairygui.js
@@ -1,4 +1,4 @@
-window.fgui = fgui;
+window.fgui = {};
 window.__extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
