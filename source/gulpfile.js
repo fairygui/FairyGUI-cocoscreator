5e28dca73ae6ca9dbedc4ebd71316bb65a46723a
weihua <weihua@hog16.com>
2020-05-13 22:40:18 +0800

    升级demo引擎 修改gulp版本。 修改npm打包脚本。

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,28 +1,28 @@
 'use strict';
 const gulp = require("gulp");
-const minify = require('gulp-minify');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 const inject = require('gulp-inject-string');
 
-gulp.task('buildJs', () => {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(inject.replace('var fgui;', ''))
-        .pipe(inject.prepend('window.fgui = {};\nwindow.fairygui = window.fgui;\n'))
-        .pipe(inject.replace('var __extends', 'window.__extends'))
-        //.pipe(minify({ ext: { min: ".min.js" } }))
-        .pipe(gulp.dest('./bin'));
-})
 
-gulp.task("buildDts", ["buildJs"], () => {
-    return tsProject.src()
-        .pipe(tsProject())
-        .dts.pipe(inject.append('import fairygui = fgui;'))
-        .pipe(gulp.dest('./bin'));
-});
+exports.buildJs = (done)=>{
+    
+    tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(inject.replace('var fgui;', ''))
+    .pipe(inject.prepend('window.fgui = {};\nwindow.fairygui = window.fgui;\n'))
+    .pipe(inject.replace('var __extends', 'window.__extends'))
+    .pipe(gulp.dest('./bin'));
 
-gulp.task("build", ["buildDts"], () => {
-    return gulp.src('bin/**/*')
-        .pipe(gulp.dest('../demo/assets/Script/Lib/'))
-});
+    done();
+};
+
+exports.buildDts = (done)=>{
+
+    tsProject.src()
+    .pipe(tsProject())
+    .dts.pipe(inject.append('import fairygui = fgui;'))
+    .pipe(gulp.dest('./bin'));
+
+    done();
+};
